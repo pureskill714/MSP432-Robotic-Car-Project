8cea4087f2f21f88cd31602819608ba27f1ae49e
pureskill714 <raheemrocker@gmail.com>
2021-12-06 23:50:00 +0800

    ultrasonic added. Final version

--- main.c
+++ main.c
@@ -1,3 +1,4 @@
+
 /* --COPYRIGHT--,BSD
  * Copyright (c) 2017, Texas Instruments Incorporated
  * All rights reserved.
@@ -47,6 +48,11 @@
  *                 If LED is red, car will travel 80 steps
  *
  *
+ *             4. Pressing Switch P1.1 will toggle between 60% PWM and 85% PWM
+ *                If LED P1.0 is red, PWM is set to 85%
+ *                If LED P1.0 is off, PWM is set to 60%
+ *
+ *
  *                MSP432P401
  *             ------------------
  *         /|\|      P3.2/UCA2RXD|<----Bluetooth Serial Terminal
@@ -57,7 +63,7 @@
  *            |             P2.5 |--> Output PWM for right motor
  *            |                  |
  *            |                  |
- * Author: 
+ * Author:
 *******************************************************************************/
 /* DriverLib Includes */
 #include <ti/devices/msp432p4xx/driverlib/driverlib.h>
@@ -72,6 +78,119 @@ volatile static uint32_t counterB = 0;
 volatile static uint32_t distanceMode = 0;
 volatile static uint32_t speedMode = 0;
 volatile static uint32_t stoppingDistance = 40;
+
+
+#define MIN_DISTANCE    15.0f
+#define TICKPERIOD      1000
+uint32_t SR04IntTimes;
+
+static void Delay(uint32_t loop)
+{
+    volatile uint32_t i;
+
+    for (i = 0 ; i < loop ; i++);
+}
+
+// -------------------------------------------------------------------------------------------------------------------
+
+void Initalise_HCSR04(void)
+{
+    /* Timer_A UpMode Configuration Parameter */
+    const Timer_A_UpModeConfig upConfig =
+    {
+            TIMER_A_CLOCKSOURCE_SMCLK,              // SMCLK Clock Source
+            TIMER_A_CLOCKSOURCE_DIVIDER_24,          //
+            TICKPERIOD,                             // 1000 tick period
+            TIMER_A_TAIE_INTERRUPT_DISABLE,         // Disable Timer interrupt
+            TIMER_A_CCIE_CCR0_INTERRUPT_ENABLE ,    // Enable CCR0 interrupt
+            TIMER_A_DO_CLEAR                        // Clear value
+    };
+
+    int a = CS_getSMCLK();
+
+    /* Configuring P3.6 as Output */
+    GPIO_setAsOutputPin(GPIO_PORT_P3, GPIO_PIN6);//
+    GPIO_setOutputLowOnPin(GPIO_PORT_P3, GPIO_PIN6);//
+
+    GPIO_setAsInputPinWithPullDownResistor(GPIO_PORT_P3, GPIO_PIN7);
+
+
+    /* Configuring Timer_A1 for Up Mode */
+    Timer_A_configureUpMode(TIMER_A1_BASE, &upConfig);
+
+    /* Enabling interrupts and starting the timer */
+    Interrupt_enableInterrupt(INT_TA1_0);
+
+    Timer_A_clearTimer(TIMER_A1_BASE);
+
+}
+
+// -------------------------------------------------------------------------------------------------------------------
+
+void TA1_0_IRQHandler(void)
+{
+    /* Increment global variable (count number of interrupt occurred) */
+    SR04IntTimes++;
+
+    /* Clear interrupt flag */
+    Timer_A_clearCaptureCompareInterrupt(TIMER_A1_BASE, TIMER_A_CAPTURECOMPARE_REGISTER_0);
+}
+
+// -------------------------------------------------------------------------------------------------------------------
+
+static uint32_t getHCSR04Time(void)
+{
+    uint32_t pulsetime=0;
+
+    /* Number of times the interrupt occurred (1 interrupt = 1000 ticks)    */
+    pulsetime = SR04IntTimes * TICKPERIOD;
+
+    /* Number of ticks (between 1 to 999) before the interrupt could occur */
+    pulsetime += Timer_A_getCounterValue(TIMER_A1_BASE); //like the remaining number of ticks
+
+    /* Clear Timer */
+    Timer_A_clearTimer(TIMER_A1_BASE);
+
+    Delay(3000);
+
+    return pulsetime;
+}
+
+// -------------------------------------------------------------------------------------------------------------------
+
+float getHCSR04Distance(void)
+{
+    uint32_t pulseduration = 0;
+    float calculateddistance = 0;
+
+    /* Generate 10us pulse at P3.6 */
+    GPIO_setOutputHighOnPin(GPIO_PORT_P3, GPIO_PIN6);
+    Delay(30);
+    GPIO_setOutputLowOnPin(GPIO_PORT_P3, GPIO_PIN6);
+
+    /* Wait for positive-edge */
+    while(GPIO_getInputPinValue(GPIO_PORT_P3, GPIO_PIN7) == 0);
+
+    /* Start Timer */
+    SR04IntTimes = 0;
+    Timer_A_clearTimer(TIMER_A1_BASE);
+    Timer_A_startCounter(TIMER_A1_BASE, TIMER_A_UP_MODE);
+
+    /* Detects negative-edge */
+    while(GPIO_getInputPinValue(GPIO_PORT_P3, GPIO_PIN7) == 1);
+
+    /* Stop Timer */
+    Timer_A_stopTimer(TIMER_A1_BASE);
+
+    /* Obtain Pulse Width in microseconds */
+    pulseduration = getHCSR04Time();
+
+    /* Calculating distance in cm */
+    calculateddistance = (float)pulseduration / 58.0f;
+
+    return calculateddistance;
+}
+// -------------------------------------------------------------------------------------------------------------------
 
 void moveForward(){
    /* Motor A move forward direction */
@@ -295,6 +414,8 @@ int main(void)
     /* Setting DCO to 24MHz*/
     CS_setDCOCenteredFrequency(CS_DCO_FREQUENCY_24);
 
+    Initalise_HCSR04();
+
     /* Configuring all LEDs as outpin pins */
     GPIO_setAsOutputPin(GPIO_PORT_P1, GPIO_PIN0);
     GPIO_setAsOutputPin(GPIO_PORT_P2, GPIO_PIN0);
@@ -369,11 +490,21 @@ int main(void)
 
     while(1)
         {
+
         //use this to test what is the new generate pwm by uncommenting and printf statement in carPID fucntion
-    //    CarPID(22,18);
+        //CarPID(22,18);
 
+        Delay(3000);
+           //Obtain distance from HCSR04 sensor and check if its less then minimum distance//
+          float calculateddistance = getHCSR04Distance();
+          printf("%.2f\n",calculateddistance);
+          if((getHCSR04Distance() < MIN_DISTANCE)){
+              stop();
+              GPIO_setOutputHighOnPin(GPIO_PORT_P1, GPIO_PIN0);
+          }
+          else{
+              GPIO_setOutputLowOnPin(GPIO_PORT_P1, GPIO_PIN0);}
         }
-
 }
 
 /*Interrupt Service Routine for UART (Bluetooth) */
@@ -431,12 +562,10 @@ void PORT1_IRQHandler(void){
     else if (GPIO_getInterruptStatus(GPIO_PORT_P1, GPIO_PIN1) != 0){
            speedMode+=1;
            if (speedMode%3 == 1){
-               GPIO_setOutputHighOnPin(GPIO_PORT_P1, GPIO_PIN0);
                rightWheel.dutyCycle = 8500;
                leftWheel.dutyCycle = 8500;}
 
            if (speedMode%2 == 0){
-              GPIO_setOutputLowOnPin(GPIO_PORT_P1, GPIO_PIN0);
               rightWheel.dutyCycle = 6000;
               leftWheel.dutyCycle = 6000;}
 
@@ -524,6 +653,3 @@ void PORT4_IRQHandler(void){
         GPIO_clearInterruptFlag(GPIO_PORT_P4, GPIO_PIN1);
     }
 }
-
-
-
